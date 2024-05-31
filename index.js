6fe3d11e2ce092561104d6e1e0c28f20aab437a2
Nguyen Tran <khoa2042002@gmail.com>
2024-05-31 16:00:15 -0700

    routes

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express =require('express');
 const app=express();
 const port=3000;
+const routeClient= require("./routers/client/index.route")
 app.set('views',"./views");
 app.set('view engine','pug');
-app.get('/',(req,res)=>{
-    res.render('client/pages/home/index');
+routeClient.index(app);
 
-});
-app.get('/product',(req,res)=>{
-    res.render('client/pages/products/index');
-});
 app.listen(port,()=>{
     console.log('running')
 });
